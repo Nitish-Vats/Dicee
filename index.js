0651c39337a7fb57b52a32eaf1782806de0d1f03
Nitish Vats <97188461+Nitish-Vats@users.noreply.github.com>
2022-12-04 22:20:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomDiceeImage="dice"+randomNumber1+".png";
-var randomDiceImageSource="Images/"+randomDiceeImage;
+var randomDiceImageSource="images/"+randomDiceeImage;
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomDiceImageSource);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImageSource="Images/dice"+randomNumber2+".png";
+var randomImageSource="images/dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource);
 
 if(randomNumber1>randomNumber2)
